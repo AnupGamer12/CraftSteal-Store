089e4d5c3ad71b7e08aa33c0e9ac235830d68fb1
AnupGamer12 <97493797+AnupGamer12@users.noreply.github.com>
2024-06-18 15:55:19 +0545

    Update app.config.ts

--- app.config.ts
+++ app.config.ts
@@ -2,21 +2,21 @@
 // See index.d.ts for the full list of available options
 export default defineAppConfig({
     // Shows in the <title> tag before the name of the page
-    titlePrefix: "Example Store",
+    titlePrefix: "CraftSteal Store",
     // The name of your store
-    storeName: "Example Store",
+    storeName: "CraftStore Store",
     // Require the user to login before they add items?
     auth: true,
     // Use external auth methods, e.g. CFX, Steam (Set to false for minecraft stores)
     authExternal: true,
     // The unique ID of the store
-    storeId: 45295,
+    storeId: 1438365,
     // A link to your main site (optional)
-    mainSiteUrl: "https://example.com",
+    mainSiteUrl: "https://craftsteal.me",
     // A discord invite url (optional)
-    discordUrl: "https://discord.gg/example",
+    discordUrl: "https://discord.craftsteal.me",
     // A IP of your server which to display (optional)
-    serverIp: "play.example.com",
+    serverIp: "play.craftsteal.me",
     // Reveal the sidebar when an item is added to the cart?
     showCartOnAdd: true,
     // Show confetti after checkout?
